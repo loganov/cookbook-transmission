dab8f6e635702eca1313ca45de1ead192ca1d217
Gregory Weaver <greg.weaver@sephora.com>
2014-05-14 12:07:00 -0700

    Adding loganov-user wrapper around /user.

---
+++ cookbooks/loganov-user/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: loganov-user
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'user'
+
+user_account 'gweaver' do
+  action [:create, :manage]
+  comment 'Gregory Weaver'
+  ssh_keys ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWpnP1ycaWA9Ha9ygcojGONHLpBCIaqOnCD562Dg0SLZ0v/eIaxsLttAbj5O087EMnU2JzjrKoVl9Bwn1Q21H0RVCTvzS9L5ofr3kNGYixZ9XpWfiEKO7OHx3wCFveOiNYqXQW0bQEWrgjZtnCrKwn+vcNqp6NDCnPjUKyeT8ClbcVCUGiOEBGo58J7A3i7ggEBmaUdcfcx8ecyxQjz2sv8fTE8DXnY3/2+HUVruHh7JfFBiwXtTpwYylitFcQd4Iz6n+QYSNrlgVknk6R9zx8DTMqYixX5DuyDRLjG50TJztnfS/cCcox4iontgHgVBzbve6x9Vv9c5m13n7XLybN gregory.weaver@gmail.com']
+  home '/home/gweaver'
+end
